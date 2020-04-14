b4f3e0aabd92163b7573fef047f8286d87aafa00
John LaDeur <jdladeur@gmail.com>
2020-04-13 19:49:46 -0500

    First commit

---
+++ game.js
@@ -0,0 +1,13 @@
+var buttonColors = [
+    "red",
+    "blue",
+    "green",
+    "yellow"
+]
+
+var randomChosenColor = randomNumber + buttonColors[i]
+
+function nextSequence(){
+    var randomNumber = Math.floor(Math.random()* 4);
+   console.log(randomNumber)
+}
