8b7cb8b800253c3012a6a93c391bd5d9f5a6e591
John LaDeur <jdladeur@gmail.com>
2020-04-13 20:26:06 -0500

    Added onclick event for red btn

--- game.js
+++ game.js
@@ -1,13 +1,21 @@
+
+
+var gamePattern = [randomChosenColor];
+
 var buttonColors = [
     "red",
     "blue",
     "green",
     "yellow"
-]
+];
 
-var randomChosenColor = randomNumber + buttonColors[i]
-
+var randomChosenColor =  buttonColors[randomNumber];
+var randomNumber = Math.floor(Math.random()* 4);
 function nextSequence(){
-    var randomNumber = Math.floor(Math.random()* 4);
+    var randomNumber
    console.log(randomNumber)
-}
+};
+
+$("#red").on("click", function(){
+    $("#red").fadeOut(25).fadeIn(25).fadeOut(25).fadeIn(25);
+});
