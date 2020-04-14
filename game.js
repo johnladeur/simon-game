c4026e790d2f049c771bf56b6e8c1d3c66eb7e5e
John LaDeur <jdladeur@gmail.com>
2020-04-14 09:47:30 -0500

    add sound for game sequence and button clicks

--- game.js
+++ game.js
@@ -1,22 +1,32 @@
 
+var buttonColors = ["red","blue","green","yellow"];
 
-var gamePattern = [randomChosenColor];
+var gamePattern = [];
 
-var buttonColors = [
-    "red",
-    "blue",
-    "green",
-    "yellow"
-];
+var userClickedPattern = [];
+
+$(".btn").click(function(e){
+    var userChosenColor = $(this).attr("id"); 
+    userClickedPattern.push(userChosenColor);
+    console.log(userClickedPattern);
+    playSound(userChosenColor);
+});
 
 function nextSequence(){
-var randomNumber = Math.floor(Math.random()* 4);
-var randomChosenColor =  buttonColors[randomNumber];
-gamePattern.push(randomChosenColor);
+    var randomNumber = Math.floor(Math.random()* 4);
+    var randomChosenColor =  buttonColors[randomNumber];
+    gamePattern.push(randomChosenColor);
 
 $("#" + randomChosenColor).fadeIn(100).fadeOut(100).fadeIn(100);
 
-var audio = new Audio('sounds/' + randomChosenColor + '.mp3');
+playSound(randomChosenColor);
+
+};
+
+function playSound(name){
+
+var audio = new Audio('sounds/' + name + '.mp3');
 audio.play();
 
 };
+
